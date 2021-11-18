d411051d1eaf656848f4b488e1fcf2dd6db55b3a
pashtet <pipochinka@edu.hse.ru>
2021-11-18 22:56:37 +0300

    Finish commit

---
+++ src/main/java/HomeWorkApp.java
@@ -0,0 +1,49 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = -21;
+        int b = 20;
+
+        if(a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = -1;
+
+        if(value <= 0) {
+            System.out.println("Красный");
+        } else if(value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 30;
+        int b = 20;
+
+        if(a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
